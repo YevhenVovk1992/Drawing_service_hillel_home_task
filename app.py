c308e2ffc2a17daed51e948e31bf7a787816da74
Vovk <vovk.valki@ukr.net>
2022-09-11 18:03:16 +0300

    Add error pages
    create css styles;
    add file_error.html and get_image_success.html;

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from werkzeug.utils import secure_filename
 
 from celery_worker import do_image_512
 
-
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/uploads')
 SECRET_KEY = "super_secret_key"
@@ -19,6 +18,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/')
 def index():
     return redirect(url_for('get_image'))
@@ -32,17 +32,23 @@ def get_image():
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
-            return 'file not in request.files'
+            return render_template(
+                'file_error.html', title="Error", error_messange='No file part'
+            )
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
             flash('No selected file')
-            return 'No selected file'
+            return render_template(
+                'file_error.html', title="Error", error_messange='No selected file'
+            )
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(join(app.config['UPLOAD_FOLDER'], filename))
-            return 'OK'
+            path_file = join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path_file)
+            do_image_512.apply_async(args=[path_file])
+            return render_template('get_image_success.html', title="Download file")
 
 
 if __name__ == '__main__':
--- celery_worker.py
+++ celery_worker.py
@@ -4,15 +4,17 @@ import os
 from PIL import Image
 from celery import Celery
 
-app = Celery('celery_worker', broker='')
+app = Celery('celery_worker', broker='pyamqp://guest@localhost//')
 
 
 @app.task
-def do_image_512(url):
+def do_image_512(path):
     size = 512, 512
-
-    for infile in glob.glob(url):
+    for infile in glob.glob(path):
         file, ext = os.path.splitext(infile)
         with Image.open(infile) as im:
             im.thumbnail(size)
             im.save(file + "MINI.jpg", "JPEG")
+    return 'Successfully'
+
+
